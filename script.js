882860063a2ebe41332a5fcef130f1c3517a9633
Kristen Hare <sailorgoon@gmail.com>
2018-04-15 23:00:26 -0500

    base mode done

--- script.js
+++ script.js
@@ -3,27 +3,25 @@ $(document).ready(onReady);
 let clickTotal = 0;
 
 function onReady (){
-    console.log( 'jquery has been loaded' );
+    console.log('jquery has been loaded');
     $('#addEmployeeButton').on('click', clickHandler);
 }
 
-
-
 function clickHandler() {
-    console.log('counter button was clicked');
+    console.log('button was clicked');
     clickTotal += Number($('#annualSalary').val()) /12;
     console.log( clickTotal );
     addNewEmployee();
     updateTotal();
     exceedMonthly();
-    $("#firstName").val(" ");
+    $('input').val(' ');
 }
 
 function addNewEmployee(){
     console.log('in addNewEmployee');
     $('#employeeList').append(
         '<tr>' +
-        '<td>' +$('#firstName').val () + '</td>' +
+        '<td>' +$('#firstName').val() + '</td>' +
         '<td>' +$('#lastName').val() + '</td>' +
         '<td>' +$('#id').val() + '</td>' +
         '<td>' +$('#title').val() + '</td>' +
@@ -33,13 +31,13 @@ function addNewEmployee(){
 }
 
 function updateTotal(){
-    $('#totalDiv').text( clickTotal );
+    $('#totalDiv').text(clickTotal.toFixed(2));
 }
 
 function exceedMonthly(){
     if( clickTotal >= 20000 ){
-        console.log( 'exceeded 20000' );
-        $('#totalDiv').css("background-color","rgb(230,150,140");
+        console.log('exceeded 20000');
+        $('#totalDiv').css("background-color","rgb(230, 150 ,140)");
     }
 }
 
